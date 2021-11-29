285ec8dfad2e6d45b3f8222abb9b991aef78cfb5
Beijiang <1808465667@qq.com>
2021-11-29 09:32:50 +0800

    Reconstruct socket_client.py

--- socket_client.py
+++ socket_client.py
@@ -6,42 +6,81 @@ import logging
 from socket import *
 
 sensor_nums = 30
-config_data = {}
 
 
-def generate_random_str(randomlength=10):
-    random_str = ''
-    base_str = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789'
-    length = len(base_str) - 1
-    for i in range(randomlength):
-        import random
-        random_str += base_str[random.randint(0, length)]
-    return random_str
+class sensor:
+    no_respond_count = 0
+    next_ret_type = 0
 
-
-def sensor_data_gen(self_id):
-    self_sn = generate_random_str(10)
-    while True:
+    def __init__(self, self_id):
+        self.self_sn = self.generate_random_str()
+        self.self_id = self_id
         with open("config.json", 'r') as config:
-            config_data = json.loads(config.read())
+            self.config_data = json.loads(config.read())
             config.close()
-        tcp_socket_client = socket(AF_INET, SOCK_STREAM)
-        dest_ip = config_data["ip"]
-        dest_port = int(config_data["port"])
-        try:
-            tcp_socket_client.connect((dest_ip, dest_port))
-            data_info = {"type": "data", "id": str(self_id), "sn": self_sn,
-                         "power": str(random.randint(10, 2000) + random.randint(0, 99) / 100), "state": "on",
+        # Read json config
+        self.tcp_socket_client = socket(AF_INET, SOCK_STREAM)
+        # Initialize a socket connection
+
+    @staticmethod
+    def generate_random_str(random_length=10):
+        random_str = ''
+        base_str = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789'
+        length = len(base_str) - 1
+        for i in range(random_length):
+            import random
+            random_str += base_str[random.randint(0, length)]
+        return random_str
+
+    # Generate total info for sensor
+    @staticmethod
+    def generate_sensor_info(self_id, self_sn, ret_type=0):
+        if ret_type == 0:
+            data_info = {"type": "data",
+                         "id": str(self_id),
+                         "sn": self_sn,
+                         "power": str(random.randint(10, 2000) + random.randint(0, 99) / 100),
+                         "state": "on",
                          "time": str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))}
             data_info_str = json.dumps(data_info, separators=(',', ':'))
-            tcp_socket_client.send(data_info_str.encode('utf-8'))
-            data_re = tcp_socket_client.recv(1024)
-            logging.info(data_info)
-            logging.info(data_re.decode('utf-8'))
-        except ConnectionRefusedError as e:
-            logging.error(e)
-            continue
-        time.sleep(int(config_data["interval"]))
+            return data_info_str
+        elif ret_type == 1:
+            data_info = {"type": "data",
+                         "id": str(self_id),
+                         "sn": self_sn,
+                         "state": "suspend",
+                         "time": str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))}
+            data_info_str = json.dumps(data_info, separators=(',', ':'))
+            return data_info_str
+        else:
+            data_info = {"type": "data",
+                         "id": str(self_id),
+                         "sn": self_sn,
+                         "state": "off",
+                         "time": str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))}
+            data_info_str = json.dumps(data_info, separators=(',', ':'))
+            return data_info_str
+
+    # Analyse server info
+    def ret_json_load(self, data_re):
+        if not data_re:
+            self.no_respond_count += 1
+            if self.no_respond_count >= 1:
+                return 1
+
+    def sensor_data_gen(self, self_id):
+        while True:
+            try:
+                self.tcp_socket_client.connect((self.config_data["ip"], int(self.config_data["port"])))
+                self.tcp_socket_client.send(self.generate_sensor_info(self_id, self.self_sn, self.next_ret_type).encode(
+                    'utf-8'))
+                data_re = self.tcp_socket_client.recv(1024)
+                # logging.info(data_re.decode('utf-8'))
+                self.ret_json_load(data_re.decode('utf-8'))
+                time.sleep(int(self.config_data["interval"]))
+            except WindowsError as e:
+                logging.error(e)
+                continue
 
 
 if __name__ == "__main__":
@@ -49,5 +88,5 @@ if __name__ == "__main__":
     logging.warning("Starting client emulator.")
     logging.warning("Client numbers: " + str(sensor_nums))
     for num in range(sensor_nums):
-        p = multiprocessing.Process(target=sensor_data_gen, args=(num,))
+        p = multiprocessing.Process(target=sensor, args=(num,))
         p.start()
