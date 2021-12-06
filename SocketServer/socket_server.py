b01236fa721647e21d5ebf082f7a189056585d68
Beijiang <1808465667@qq.com>
2021-12-06 18:59:49 +0800

    Reconstruct socket_client.py

--- SocketServer/socket_server.py
+++ SocketServer/socket_server.py
@@ -1,13 +1,23 @@
 import socketserver
+import threading
 import time
+
+print_mutex = threading.Lock()
 
 
 class MyServer(socketserver.BaseRequestHandler):
     def handle(self):
         while True:
             data_received = self.request.recv(1024).strip()
+            print_string_(data_received)
             self.request.sendall(b"{\"result\":\"ok\"}")
-            time.sleep(0.1)
+            time.sleep(0.01)
+
+
+def print_string_(data):
+    print_mutex.acquire()
+    print(data)
+    print_mutex.release()
 
 
 def run():
